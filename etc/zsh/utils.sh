027431049518d3eb2d9b0aef9314966fa281f68e
Junyi Hou <hji200914@gmail.com>
2024-11-07 19:58:03 +0800

    fix: fzf caused "unknown option: --zsh"

--- etc/zsh/utils.sh
+++ etc/zsh/utils.sh
@@ -11,9 +11,7 @@ else
 fi
 
 if command_exists fzf; then
-    # Set up fzf key bindings and fuzzy completion
-    source <(fzf --zsh)
-    echo "\033[32m\033[0;39m  fzf activated"
+    echo "\033[32m\033[0;39m  fzf was found"
 else
     echo "\033[31m\033[0;39m  fzf not found"
 fi
